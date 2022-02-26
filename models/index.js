4c52645b0074e4e45a5689e01db663dfc9338185
EzequielMejiasMelo <ezequiel.mejiasmelo@gmail.com>
2022-02-25 23:01:24 -0500

    Finished model relations

--- models/index.js
+++ models/index.js
@@ -3,8 +3,28 @@ const Post = require('./Post');
 const User = require('./User');
 
 User.hasMany(Post, {
-  fore
-})
+  foreignKey: 'userId'
+});
+
+Post.belongsTo(User, {
+  foreignKey: 'userId'
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'userId'
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'userId'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'postId'
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'postId'
+});
 
 module.exports = {
   User,
