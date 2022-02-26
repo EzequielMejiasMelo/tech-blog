7f97f96cf3e6af65cea955e11c3137120ea3855b
EzequielMejiasMelo <ezequiel.mejiasmelo@gmail.com>
2022-02-25 22:19:36 -0500

    Added sequelize

--- server.js
+++ server.js
@@ -9,9 +9,19 @@ const PORT = process.env.PORT || 3001;
 
 
 
-// Need a functionality do we need here?
+// Need a functionality do we need here? add sequelize
+const sequelize = require('./config/config');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUnInitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+}
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
